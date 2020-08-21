49e58f6908c927e6d3dd46c189c1dab5c4bb3193
Petr Chromec <mortal_flesh@seznam.cz>
2020-08-21 12:43:40 +0200

    Test parsing tuc parts

--- tests/Tuc/ParserTest.fs
+++ tests/Tuc/ParserTest.fs
@@ -53,6 +53,71 @@ module Domain =
         |> orFail (List.map TypeName.value >> String.concat "\n  - " >> sprintf "Unresolved types:\n%s")
 
 [<RequireQualifiedAccess>]
+module Parts =
+    open MF.Domain
+
+    let path = "./Tuc/Fixtures/parts"
+
+    type Case = {
+        Description: string
+        Tuc: string
+        Expected: Result<string, ParseError>
+    }
+
+    let case description tuc expected =
+        {
+            Description = description
+            Tuc = path </> tuc
+            Expected = expected |> Result.map ((</>) path)
+        }
+
+    let provider: Case list =
+        [
+            case "Sections" "section.tuc" (Ok "section.puml")
+
+            case "Initiator's lifeline" "lifeline.tuc" (Ok "lifeline.puml")
+
+            case "Notes" "note.tuc" (Ok "note.puml")
+            case "Note without a caller" "note-without-caller.tuc" (Error <| NoteWithoutACaller (5, 0, @"""Note without caller"""))
+
+            case "Do" "do.tuc" (Ok "do.puml")
+            case "Do without a caller" "do-without-caller.tuc" (Error <| DoWithoutACaller (5, 0, "do Some stuff"))
+
+            case "Handle event in stream" "handle-event-in-stream.tuc" (Ok "handle-event-in-stream.puml")
+
+            case "Read event from stream" "read-event.tuc" (Ok "read-event.puml")
+            case "Post event to stream" "post-event.tuc" (Ok "post-event.puml")
+
+            case "Call a service method" "service-method-call.tuc" (Ok "service-method-call.puml")
+
+            case "Loops" "loop.tuc" (Ok "loop.puml")
+            case "Groups" "group.tuc" (Ok "group.puml")
+            case "If" "if.tuc" (Ok "if.puml")
+        ]
+
+    open MF.Tuc.Parser
+    open MF.Puml
+
+    let test output domainTypes { Tuc = tuc; Expected = expected; Description = description } =
+        let parsedTucs =
+            tuc
+            |> Parser.parse output domainTypes
+
+        match expected, parsedTucs with
+        | Ok expected, Ok actual ->
+            let puml =
+                actual
+                |> Generate.puml output description
+                |> orFail PumlError.format
+                |> Puml.value
+
+            expectFile expected (puml.TrimEnd().Split "\n") description
+
+        | Error expected, Error actual -> Expect.equal actual expected description
+        | Error _, Ok success -> failtestf "Error was expected, but it results in ok.\n%A" success
+        | Ok _, Error error -> failtestf "Success was expected, but it results in error.\n%A" error
+
+[<RequireQualifiedAccess>]
 module Event =
     open MF.Domain
 
@@ -139,6 +204,13 @@ let parserTests =
     let output = MF.ConsoleApplication.Output.console
 
     testList "Tuc.Parser" [
+        testCase "should parse parts" <| fun _ ->
+            let domainTypes =
+                Parts.path </> "domain.fsx"
+                |> Domain.parseDomainTypes output
+
+            Parts.provider |> List.iter (Parts.test output domainTypes)
+
         testCase "should parse events" <| fun _ ->
             let domainTypes =
                 Event.path </> "domain.fsx"
